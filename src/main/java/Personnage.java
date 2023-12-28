78b2c8b2567811b6f65828cc04472d58b00c9c5a
julie <juliecobas@outlook.com>
2023-12-28 14:11:32 +0100

    Création du personnage / Constructeur / AcheterArme

---
+++ src/main/java/Personnage.java
@@ -0,0 +1,29 @@
+import java.sql.SQLOutput;
+
+public class Personnage {
+
+    /** Variables d'instance */
+    private String nom;
+    private int barreDeVie;
+    private Arme arme;
+    private int pieces;
+
+    /** Constructeur*/
+    public Personnage(String nom){
+        this.nom=nom;
+        this.barreDeVie = 100;
+        this.pieces = 0;
+    }
+
+    public void acheterArme(Arme nouvelleArme){
+        if (pieces >= nouvelleArme.getCout()){
+            this.arme = nouvelleArme;
+            this.pieces = nouvelleArme.getCout();
+            System.out.println("Arme achetée : " + nouvelleArme.getNom());
+        }
+        else{
+            System.out.println("Pas assez de pièces pour acheter cette arme.");
+        }
+    }
+
+}
